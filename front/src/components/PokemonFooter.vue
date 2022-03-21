4bf9d779f76cb96cffc358ca1720a4076fdc19d6
nikiforov.av <nikiforov.av@dclouds.ru>
2022-03-21 15:29:44 +0700

    DCSTUD-9: added socials in footer

--- front/src/components/PokemonFooter.vue
+++ front/src/components/PokemonFooter.vue
@@ -1,43 +1,98 @@
 <template>
-  <footer class="footer">
-    <div class="footer__wrapper footer__container">
-      <ul class="footer__right-menu footer__menu">
-        <li class="footer__right-menu-item footer__item">
-          <router-link class="footer__item--link" to="/">О нас</router-link>
-        </li>
-        <li class="footer__right-menu-item footer__item">
-          <router-link class="footer__item--link" to="/">
-            Все покемоны
-          </router-link>
-        </li>
-        <li class="footer__right-menu-item footer__item">
-          <router-link class="footer__item--link" to="/">FAQ</router-link>
-        </li>
-      </ul>
-      <ul class="footer__right-menu footer__menu">
-        <li class="footer__right-menu-item footer__item">
-          <router-link class="footer__item--link" to="/">
-            Мы находимся здесь
-          </router-link>
-        </li>
-        <li class="footer__right-menu-item footer__item">
-          <router-link class="footer__item--link" to="/">
-            Смотреть сериал
-          </router-link>
-        </li>
-      </ul>
+  <footer class="pokemon-footer">
+    <div class="pokemon-footer__wrapper pokemon-footer__container">
+      <div class="pokemon-footer__menu">
+        <ul class="pokemon-footer__left-menu pokemon-footer__menu">
+          <li class="pokemon-footer__right-menu-item pokemon-footer__item">
+            <router-link class="pokemon-footer__item--link" to="/">
+              О нас
+            </router-link>
+          </li>
+          <li class="pokemon-footer__right-menu-item pokemon-footer__item">
+            <router-link class="pokemon-footer__item--link" to="/">
+              Все покемоны
+            </router-link>
+          </li>
+          <li class="pokemon-footer__right-menu-item pokemon-footer__item">
+            <router-link class="pokemon-footer__item--link" to="/">
+              FAQ
+            </router-link>
+          </li>
+        </ul>
+        <ul class="pokemon-footer__right-menu pokemon-footer__menu">
+          <li class="pokemon-footer__right-menu-item pokemon-footer__item">
+            <router-link class="pokemon-footer__item--link" to="/">
+              Мы находимся здесь
+            </router-link>
+          </li>
+          <li class="pokemon-footer__right-menu-item pokemon-footer__item">
+            <router-link class="pokemon-footer__item--link" to="/">
+              Смотреть сериал
+            </router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="pokemon-footer__socials">
+        <icon-template
+          class="pokemon-footer__socials-item"
+          name="vk"
+          width="28"
+          height="28"
+        />
+        <icon-template
+          class="pokemon-footer__socials-item"
+          name="instagram"
+          width="28"
+          height="28"
+        />
+        <icon-template
+          class="pokemon-footer__socials-item"
+          name="telegram"
+          width="28"
+          height="28"
+        />
+      </div>
     </div>
   </footer>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import { defineComponent } from 'vue'
+import IconTemplate from '@/components/IconTemplate.vue'
+
+export default defineComponent({
+  components: {
+    IconTemplate,
+  },
+})
 </script>
 
 <style scoped lang="scss">
-.footer {
+.pokemon-footer {
   padding: 12px 0;
   background-color: rgba(0, 0, 0, 0.4);
+
+  &__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+  }
+
+  &__menu {
+    display: flex;
+  }
+
+  &__left-menu {
+    display: flex;
+    flex-direction: column;
+    margin-right: 2rem;
+  }
+
+  &__right-menu {
+    display: flex;
+    flex-direction: column;
+  }
 
   &__item {
     margin: 8px 0;
@@ -50,6 +105,10 @@ export default {}
     &:hover {
       color: #eaeaea;
     }
+  }
+
+  &__socials-item {
+    margin-right: 2rem;
   }
 }
 </style>
